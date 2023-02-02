629e0bbea4f67a7242b6b3b2970f85aece06ff28
dnapolitanno <dudadnapolitano@gmail.com>
2023-02-02 12:12:16 -0300

    feat: created function Getdata for list products

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/header";
+import Getdata from "./components/Productslist";
 import "./App.css";
 
 function App() {
   return (
     <section className="App">
       <Header />
+      <Getdata />
     </section>
   );
 }
--- src/components/Productslist/index.jsx
+++ src/components/Productslist/index.jsx
@@ -1,1 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+export const Api = axios.create({
+  baseURL: "https://hamburgueria-kenzie-json-serve.herokuapp.com/",
+  timeout: 5000,
+});
+
+function Getdata() {
+  const [burguerlist, setBurguerlist] = useState([]);
+
+  useEffect(() => {
+    async function renderProducts() {
+      try {
+        const response = await Api.get("products");
+        setBurguerlist(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    renderProducts();
+  }, []);
+
+  return (
+    <div>
+      {burguerlist.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
+    </div>
+  );
+}
+
+export default Getdata;
