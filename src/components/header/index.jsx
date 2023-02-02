0cce174f6b202bf90639fd83dc051012494a13ad
dnapolitanno <dudadnapolitano@gmail.com>
2023-02-01 21:33:58 -0300

    feat: created header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,1 +1,24 @@
+import React from "react";
+import Logokenzie from "../Imglogo";
+import "./index.css";
 
+const Header = () => {
+  return (
+    <>
+      <header>
+        <section className="headercontainer container">
+          <Logokenzie />
+          <nav>
+            <ul>
+              <li>
+                <input type="text" placeholder="Digitar Pesquisa" />
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </header>
+    </>
+  );
+};
+
+export default Header;
