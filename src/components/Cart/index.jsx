0587ef1990517be1ace5c815e8024b4d0e468ce3
dnapolitanno <dudadnapolitano@gmail.com>
2023-02-03 14:21:47 -0300

    feat: create cart

---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Emptylist from "./Emptylist";
+import { Cartcontainer } from "./style";
+
+export const Cart = () => {
+  return (
+    <>
+      <Cartcontainer>
+        <div>
+          <h3>Carrinho de compras</h3>
+        </div>
+      </Cartcontainer>
+    </>
+  );
+};
---
+++ src/components/Cart/style.js
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const Cartcontainer = styled.section``;
